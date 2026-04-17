6815445e9d20d9f444384ed1bbb257b297da2bd9
Arjun Biju <arjunan8080@gmail.com>
2026-04-17 00:40:12 -0400

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.ts
@@ -0,0 +1,9 @@
+// Last updated: 4/17/2026, 12:40:12 AM
+function runningSum(nums: number[]): number[] {
+    let sumarray = [nums[0]];
+    for(let i=1;i<nums.length;i++)
+    {
+        sumarray[i] = sumarray[i-1] + nums[i];
+    }
+    return sumarray;
+};
